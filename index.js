2ad11880d1e04655e622af3caa0973ee3b9e66ee
Christina Burchette <burchc426@gmail.com>
2018-12-11 01:20:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 /* Enter the code to remove the main node element under this comment */
+document.querySelector('main')
+var maindiv = document.querySelector('main')
+maindiv.remove()
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement("H1");
+newHeader.setAttribute("id", "victory")
+
+let H1 = "Christina is the champion"
