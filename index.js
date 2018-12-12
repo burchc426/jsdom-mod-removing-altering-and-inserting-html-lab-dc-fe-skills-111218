fc56779794ad223c7ce048fa88226e73a219b95d
Christina Burchette <burchc426@gmail.com>
2018-12-12 01:38:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
 document.querySelector('main')
+<<<<<<< HEAD
 let maindiv = document.querySelector('main')
 maindiv.remove()
 
@@ -8,3 +9,13 @@ const newHeader = document.createElement("h1");
 document.body.appendChild(newHeader)
 newHeader.setAttribute("id", "victory")
 newHeader.innerText = "Christina is the champion"
+=======
+var maindiv = document.querySelector('main')
+maindiv.remove()
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("H1");
+newHeader.setAttribute("id", "victory")
+
+let H1 = "Christina is the champion"
+>>>>>>> 2ad11880d1e04655e622af3caa0973ee3b9e66ee
